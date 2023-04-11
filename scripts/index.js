5a051cc2afc26967912dcb2bd97a745be0d58f14
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-11 15:45:45 -0300

    add: flip cards function, set icons png

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card"
+const ICON = "icon"
 
 let teams = ['lakers','celtics', 'gsw', 'bulls', 'hornets',
     'nets','cavs', 'mavs', 'hawks', 'knicks', 'spurs', 'wolves'
@@ -11,7 +13,7 @@ let teams = ['lakers','celtics', 'gsw', 'bulls', 'hornets',
 // ----- Functions -----
 
 
-// ----- Start Game Function -----
+// ----- Start Game Functions -----
 
 let cards = null;
 
@@ -20,6 +22,7 @@ function startGame(){
     cards = createCardsFromTeams(teams);
     shuffleCards(cards);
     console.log(cards);
+    initializeCards(cards);
 }
 
 function shuffleCards(cards){
@@ -27,25 +30,48 @@ function shuffleCards(cards){
     let randomIndex = 0;
 
     while(currentIndex !== 0){
-        randomIndex = Math.floor(Math.random() * currentIndex)
+        randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
         
         // invertendo/trocando valores de duas variaveis
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
     }
 }
 
+function initializeCards(cards){
+    let gameBoard = document.querySelector("#gameBoard");
+    console.log(gameBoard);
 
-// ----- Create Cards Function -----
+    cards.forEach(card => {
+        let cardElement = document.createElement("div");
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD, "brdr_radius");
+
+        // Verificando se os icons das cards sao iguais
+        cardElement.dataset.icon = card.icon;
+
+        // Event listener de jogada
+        cardElement.addEventListener('click', flipCard)
+
+        // Colocando card no tabuleiro
+        gameBoard.appendChild(cardElement);
+
+        createCardContent(card, cardElement);
+
+    })
+}
+
+
+// ----- Create Cards Functions -----
 
 createCardsFromTeams(teams)
 function createCardsFromTeams(teams){
 
     let cards = [];
 
-    for (let team of teams){
+    teams.forEach((team)=>{
         cards.push((createPairFromTeam(team)));
-    }
+    })
     return (cards.flatMap(pair => pair)); // "desmembra" um item do array e o retorna no array.
 }
 
@@ -63,4 +89,30 @@ function createPairFromTeam(team){
 
 function createIdWithTeam(team){
     return team + parseInt(Math.random() * 1000) ;
+}
+
+function createCardContent(card, cardElement){
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+// ----- Colocando imagem do time ou </> no card -----
+function createCardFace(face, card, element){
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face)
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+
+    element.appendChild(cardElementFace);
+}
+
+// ----- Flip Card Function ----- 
+function flipCard(card){
+    this.classList.add("flip");
 }
