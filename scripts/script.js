f852516e59c30599f0c75731ef77ba3ad75ec67d
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-13 20:10:13 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -108,7 +108,7 @@ function hide(){
 }
 
 easyModeBtn.addEventListener("click", ()=> {
-    game.teams = ['lakers','celtics', 'gsw', 'bulls', 'heat','nets','cavs', 'mavs']
+    game.teams = ['lakers','magic', 'gsw', 'bulls', 'heat','nets','cavs', 'mavs']
     gameBoard.innerHTML = "";
     startGame();
     gameBoard.style.gridTemplateColumns = " 120px 120px 120px 120px"
